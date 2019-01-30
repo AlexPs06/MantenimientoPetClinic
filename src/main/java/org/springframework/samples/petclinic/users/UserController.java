7a550e7696fd8cc84a4a70fb1b323f68a3ebc61f
AlexPs06 <163195@ids.upchiapas.edu.mx>
2019-01-29 21:20:23 -0600

    nueva clase

---
+++ src/main/java/org/springframework/samples/petclinic/users/UserController.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.springframework.samples.petclinic.users;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import org.springframework.boot.convert.DataSizeUnit;
+import org.springframework.stereotype.Controller;
+
+/**
+ *
+ * @author AlexPS
+ */
+
+
+@Controller
+public class UserController {
+    
+}
